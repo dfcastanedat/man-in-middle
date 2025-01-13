508fbf54d15390db77953f934921924c16ea01b2
Daniel Castaneda <daniel.castaneda@proxet.com>
2025-01-12 20:05:35 -0500

    aiohttp

--- translatorV1/pokeapi/views.py
+++ translatorV1/pokeapi/views.py
@@ -1,25 +1,32 @@
+import aiohttp
+import asyncio
 import requests
+import json
 
-from rest_framework.views import APIView
+from adrf.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from multiprocessing import Process, cpu_count
 
 # Create your views here.
 class PokemonListView(APIView):
     _pokeapi_url = "https://pokeapi.co/api/v2/pokemon"
 
-    def _parsePokemonList(self, pokemonObject):
+    async def _parsePokemonList(self, session: aiohttp.ClientSession, pokemonObject):
+        print(pokemonObject)
         url = pokemonObject.get("url")
         if(url == None):
             return
         try:
-            response = requests.get(url)
-            response.raise_for_status()
+            async with session.get(url) as response:
+            # Raise an exception for HTTP errors (4xx, 5xx)
+                stream = response.content
+                streamData = await stream.read()
+                response.raise_for_status()
         except requests.exceptions.RequestException as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        data = response.json()
+        decodedData = streamData.decode('utf8').replace("'", '"')
+        data = json.loads(decodedData)
         parsed_data = {
             "sprite": data.get("sprites").get("front_default"),
             "name": data.get("name"),
@@ -29,7 +36,7 @@ class PokemonListView(APIView):
         }
         return parsed_data
 
-    def get(self, request):
+    async def get(self, request):
         try:
             # Fetch data from the PokeAPI
             response = requests.get(self._pokeapi_url, params={"limit": 20})
@@ -40,13 +47,19 @@ class PokemonListView(APIView):
         # Parse the data
         data = response.json()
         results = data.get("results", []) or []
-        parsedResults = list(map(self._parsePokemonList, results))
-        print(parsedResults)
+        
+        async with aiohttp.ClientSession() as session:
+            # Create tasks for each URL
+            tasks = [self._parsePokemonList(session, pokemon) for pokemon in results]
+            parsedPokemons = await asyncio.gather(*tasks)
+
+        print(parsedPokemons)
+
         parsed_data = {
             "count": data.get("count"),
             "next": data.get("next"),
             "previous": data.get("previous"),
-            "results": parsedResults,
+            "results": parsedPokemons,
         }
 
         return Response(parsed_data, status=status.HTTP_200_OK)
